80f4f67b2a545b948ce1c14f126f1db120ea5c15
KnockKnockP <memeboi420666@gmail.com>
2020-06-18 11:51:32 +0900

    Make the main menu.

---
+++ RigidStackSource/RigidStack/Assets/prefabs/gameManager/scripts/loadSceneScript.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class loadSceneScript : MonoBehaviour {
+    public void loadScene(string sceneName) {
+        SceneManager.LoadScene(sceneName);
+        return;
+    }
+}
