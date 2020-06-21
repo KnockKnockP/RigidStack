2f27d6d823e68dc5401ee58cf7074b9e015f5a2a
KnockKnockP <memeboi420666@gmail.com>
2020-06-21 23:24:09 +0900

    Make rotation, cancel, and confirm functions in object editing panel.

--- RigidStackSource/RigidStack/Assets/prefabs/UI/scripts/dragAndDropScript.cs
+++ RigidStackSource/RigidStack/Assets/prefabs/UI/scripts/dragAndDropScript.cs
@@ -3,17 +3,20 @@ using UnityEngine.EventSystems;
 
 public class dragAndDropScript : MonoBehaviour, IPointerDownHandler, IDragHandler, IPointerUpHandler {
     private objectClass objectImageGameObjectObjectClass;
+    public static dragAndDropScript _dragAndDropScript;
     private Camera mainCamera;
-    private GameObject placeHolderGameObject, placedGameObject, towerObjects, dockPanel, objectEditingPanel;
-    [SerializeField] private GameObject objectImageGameObject = null;
+    private GameObject placeHolderGameObject, placedGameObject;
+    [SerializeField] private GameObject objectImageGameObject = null, towerObjects = null, dockPanel = null, objectEditingPanel = null;
     [HideInInspector] public GameObject objectToPlace;
 
     private void Awake() {
         objectImageGameObjectObjectClass = objectImageGameObject.GetComponent<objectClass>();
         mainCamera = Camera.main;
-        towerObjects = GameObject.Find("towerObjects");
-        dockPanel = GameObject.Find("dockPanel");
-        objectEditingPanel = GameObject.Find("objectEditingPanel");
+        return;
+    }
+
+    private void Start() {
+        disableObjectEditingPanel();
         return;
     }
 
@@ -21,6 +24,7 @@ public class dragAndDropScript : MonoBehaviour, IPointerDownHandler, IDragHandle
         if (objectImageGameObjectObjectClass.objectCount != 0) {
             if (StaticVariables.isDragging == false) {
                 StaticVariables.isDragging = true;
+                _dragAndDropScript = this;
                 placeHolderGameObject = objectToPlace;
                 placeHolderGameObject.GetComponent<PolygonCollider2D>().enabled = false;
                 placeHolderGameObject.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeAll;
@@ -43,24 +47,40 @@ public class dragAndDropScript : MonoBehaviour, IPointerDownHandler, IDragHandle
         if (objectImageGameObjectObjectClass.objectCount != 0) {
             if (StaticVariables.isDragging == true) {
                 enableObjectEditingPanel();
+                StaticVariables.isDragging = false;
             }
         }
+        return;
+    }
+
+    public void placeObject() {
+        Rigidbody2D rigidbody2D = placedGameObject.GetComponent<Rigidbody2D>();
+        disableObjectEditingPanel();
+        objectImageGameObjectObjectClass.objectCount--;
+        placedGameObject.GetComponent<PolygonCollider2D>().enabled = true;
+        rigidbody2D.constraints = RigidbodyConstraints2D.None;
+        return;
+    }
+
+    public void cancelPlacingObject() {
+        Destroy(placedGameObject);
+        disableObjectEditingPanel();
+        return;
+    }
+
+    public void rotateLeft() {
+        placedGameObject.transform.Rotate(0f, 0f, 5f);
+        return;
+    }
+
+    public void rotateRight() {
+        placedGameObject.transform.Rotate(0f, 0f, -5f);
         return;
     }
 
     private void enableObjectEditingPanel() {
         dockPanel.SetActive(false);
         objectEditingPanel.SetActive(true);
-        return;
-    }
-
-    public void placeObject() {
-        disableObjectEditingPanel();
-        Rigidbody2D rigidbody2D = placedGameObject.GetComponent<Rigidbody2D>();
-        objectImageGameObjectObjectClass.objectCount--;
-        placedGameObject.GetComponent<PolygonCollider2D>().enabled = true;
-        rigidbody2D.constraints = RigidbodyConstraints2D.None;
-        StaticVariables.isDragging = false;
         return;
     }
 
---
+++ RigidStackSource/RigidStack/Assets/prefabs/gameManager/scripts/objectEditingScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class objectEditingScript : MonoBehaviour {
+    public void confirmPlacement() {
+        dragAndDropScript._dragAndDropScript.placeObject();
+        return;
+    }
+
+    public void cancelPlacement() {
+        dragAndDropScript._dragAndDropScript.cancelPlacingObject();
+        return;
+    }
+
+    public void rotateLeft() {
+        dragAndDropScript._dragAndDropScript.rotateLeft();
+        return;
+    }
+
+    public void rotateRight() {
+        dragAndDropScript._dragAndDropScript.rotateRight();
+        return;
+    }
+}
