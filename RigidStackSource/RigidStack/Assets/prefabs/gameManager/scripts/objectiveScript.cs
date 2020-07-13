2b376bb3e48e8d1eab52eb2048e326cee62d8723
KnockKnockP <memeboi420666@gmail.com>
2020-07-13 10:17:32 +0900

    Generate new objective.

---
+++ RigidStackSource/RigidStack/Assets/prefabs/gameManager/scripts/objectiveScript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class objectiveScript : MonoBehaviour {
+    [SerializeField] private Transform heightTextsParent = null;
+    [SerializeField] private Canvas textCanvasTemplate = null;
+    private List<Canvas> textCanvases = new List<Canvas>();
+    //private Camera mainCamera;
+
+    private void Awake() {
+        //mainCamera = Camera.main;
+        generateObjective(true);
+        return;
+    }
+
+    private void generateObjective(bool isFromAwake) {
+        if (isFromAwake == false) {
+            reset();
+            freezeAll();
+        }
+        for (long i = StaticClass.objectiveScore; i <= (StaticClass.objectiveScore + 10); i = (i + 5)) {
+            //Canvas newCanvas = Instantiate(textCanvasTemplate, new Vector2((mainCamera.transform.position.x - mainCamera.transform.localScale.x), i), Quaternion.identity, heightTextsParent);
+            Canvas newCanvas = Instantiate(textCanvasTemplate, new Vector2(0, i), Quaternion.identity, heightTextsParent);
+            newCanvas.GetComponentInChildren<Text>().text = newCanvas.transform.position.y.ToString();
+            newCanvas.gameObject.SetActive(true);
+            textCanvases.Add(newCanvas);
+        }
+        StaticClass.objectiveScore = (StaticClass.objectiveScore + 10);
+        return;
+    }
+
+    private void freezeAll() {
+        heightScript _heightScript = FindObjectOfType<heightScript>();
+        foreach (Transform placedObjectsTransform in _heightScript.placedObjectsTransforms) {
+            GameObject placedObject = placedObjectsTransform.gameObject;
+            placedObject.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeAll;
+        }
+        StaticClass.objectiveScore = _heightScript._maxHeight;
+        return;
+    }
+
+    private void reset() {
+        foreach (Canvas _textCanvas in textCanvases) {
+            Destroy(_textCanvas);
+        }
+        textCanvases.Clear();
+        return;
+    }
+}
--- RigidStackSource/RigidStack/Assets/scripts/StaticClass.cs
+++ RigidStackSource/RigidStack/Assets/scripts/StaticClass.cs
@@ -4,6 +4,7 @@ public static class StaticClass {
     public static bool isDragging, isCollisionFishy;
     public static byte angle = 1;
     public static float cameraMovementSpeed = 0.1f;
+    public static int objectiveScore;
 
     public static Color Color32ToColor(byte r, byte g, byte b) {
         return new Color((r / 255), (g / 255), (b / 255));
