4fab2e4a0a5ea99539b2c56b94f1563d2c320035
KnockKnockP <memeboi420666@gmail.com>
2020-07-22 00:04:57 +0900

    Add a fake string enum that takes care of scene names.

--- RigidStackSource/RigidStack/Assets/prefabs/gameManager/scripts/loadSceneScript.cs
+++ RigidStackSource/RigidStack/Assets/prefabs/gameManager/scripts/loadSceneScript.cs
@@ -1,9 +1,20 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
+public static class SceneNames {
+    public static readonly string MainMenu = "mainMenu";
+    public static readonly string level = "level";
+}
+
 public class loadSceneScript : MonoBehaviour {
     public void loadScene(string sceneName) {
+        StaticClass.objectiveScore = 0;
         SceneManager.LoadScene(sceneName);
+        return;
+    }
+
+    public void loadLevel() {
+        loadScene(SceneNames.level);
         return;
     }
 }
