e8ad641d34243c14dea1b202e8887753106347e2
KnockKnockP <memeboi420666@gmail.com>
2020-07-22 00:07:31 +0900

    Make a pause menu.

---
+++ RigidStackSource/RigidStack/Assets/scripts/pauseScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class pauseScript : MonoBehaviour {
+    private bool isPaused;
+    [SerializeField] private GameObject pauseMenuPanel = null;
+
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.Escape) == true) {
+            pauseOrResume();
+        }
+        return;
+    }
+
+    public void pauseOrResume() {
+        pauseMenuPanel.SetActive(!isPaused);
+        isPaused = (!isPaused);
+        return;
+    }
+
+    public void toMainMenu() {
+        FindObjectOfType<loadSceneScript>().loadScene(SceneNames.MainMenu);
+        return;
+    }
+
+    public void exit() {
+        Debug.LogWarning("Call the save function here.");
+        Application.Quit();
+        return;
+    }
+}
