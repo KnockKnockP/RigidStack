1e05df8329f27adfbaf6df36c4f45e52b6841cfa
KnockKnockP <memeboi420666@gmail.com>
2020-07-22 00:07:04 +0900

    Make objectiveScore print out what it's updated value is when being written.

--- RigidStackSource/RigidStack/Assets/scripts/StaticClass.cs
+++ RigidStackSource/RigidStack/Assets/scripts/StaticClass.cs
@@ -4,7 +4,16 @@ public static class StaticClass {
     public static bool isDragging, isCollisionFishy;
     public static byte angle = 1;
     public static float cameraMovementSpeed = 0.1f;
-    public static int objectiveScore;
+    private static int _objectiveScore;
+    public static int objectiveScore {
+        get {
+            return _objectiveScore;
+        }
+        set {
+            _objectiveScore = value;
+            Debug.Log("Objective score has been set to " + objectiveScore);
+        }
+    }
 
     public static Color Color32ToColor(byte r, byte g, byte b) {
         return new Color((r / 255), (g / 255), (b / 255));
