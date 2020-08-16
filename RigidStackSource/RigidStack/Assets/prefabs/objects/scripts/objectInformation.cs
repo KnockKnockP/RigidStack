c607e41caeaf0bfae41063664c6a8cab2270bb4c
KnockKnockP <memeboi420666@gmail.com>
2020-08-16 18:10:15 +0900

    Fix objects can fall and not hit the ground.

--- RigidStackSource/RigidStack/Assets/prefabs/objects/scripts/objectInformation.cs
+++ RigidStackSource/RigidStack/Assets/prefabs/objects/scripts/objectInformation.cs
@@ -2,4 +2,14 @@
 
 public class objectInformation : MonoBehaviour {
     public int minimumAmount = 0, maximumAmount = 0;
+
+
+    [SerializeField] private postDragAndDropScript _postDragAndDropScript = null;
+
+    private void Update() {
+        if ((_postDragAndDropScript == null) && (transform.position.y < -1f)) {
+            FindObjectOfType<endMenuManager>().endGame();
+        }
+        return;
+    }
 }
