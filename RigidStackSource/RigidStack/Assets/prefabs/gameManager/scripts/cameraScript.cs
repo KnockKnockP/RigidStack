8429a5eef23ab8a68aff993e68d7d8fbdcda9dc5
KnockKnockP <memeboi420666@gmail.com>
2020-09-16 12:24:07 +0900

    Fix camera movement speed being framerate dependant.

--- RigidStackSource/RigidStack/Assets/prefabs/gameManager/scripts/cameraScript.cs
+++ RigidStackSource/RigidStack/Assets/prefabs/gameManager/scripts/cameraScript.cs
@@ -11,7 +11,7 @@ public class cameraScript : MonoBehaviour {
 
     #region Variables for moving the camera.
     private bool shouldMoveCameraUp, shouldMoveCameraDown;
-    public static float cameraMovementSpeed = 0.1f;
+    public static float cameraMovementSpeed = 10f;
     [SerializeField] private endMenuManager _endMenuManager = null;
     [HideInInspector] public Vector3 originalCameraPosition;
     [SerializeField] private Transform platformTransform = null, girdTransform = null;
@@ -56,7 +56,7 @@ public class cameraScript : MonoBehaviour {
     private void moveCameraUp() {
         _endMenuManager.shouldMoveTheCamera = false;
         Vector3 newPosition = _sharedMonobehaviour.mainCamera.transform.position;
-        newPosition.y = (newPosition.y + cameraMovementSpeed);
+        newPosition.y = (newPosition.y + (cameraMovementSpeed * Time.unscaledDeltaTime));
         _sharedMonobehaviour.mainCamera.transform.position = newPosition;
         return;
     }
@@ -64,7 +64,7 @@ public class cameraScript : MonoBehaviour {
     private void moveCameraDown() {
         _endMenuManager.shouldMoveTheCamera = false;
         Vector3 newPosition = _sharedMonobehaviour.mainCamera.transform.position;
-        newPosition.y = (newPosition.y - cameraMovementSpeed);
+        newPosition.y = (newPosition.y - (cameraMovementSpeed * Time.unscaledDeltaTime));
         if (newPosition.y >= -2) {
             _sharedMonobehaviour.mainCamera.transform.position = newPosition;
         }
