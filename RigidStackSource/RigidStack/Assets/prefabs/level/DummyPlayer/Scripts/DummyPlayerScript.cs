0b31d77f089d62182419cfb44c1c615b1b2443fc
KnockKnockP <memeboi420666@gmail.com>
2020-10-17 00:55:37 +0900

    Simplify DummyPlayerScript.

--- RigidStackSource/RigidStack/Assets/prefabs/level/DummyPlayer/Scripts/DummyPlayerScript.cs
+++ RigidStackSource/RigidStack/Assets/prefabs/level/DummyPlayer/Scripts/DummyPlayerScript.cs
@@ -1,53 +1,15 @@
 ﻿using Mirror;
-using System.Collections;
 using UnityEngine;
 
 public class DummyPlayerScript : NetworkBehaviour {
-    public struct DummyPlayer {
-        public NetworkIdentity networkIdentity;
-        public int index;
-    }
-
-    public class DummyPlayerSyncList : SyncList<DummyPlayer> {}
-
-    //Variables for target rpc functions.
-    public DummyPlayerSyncList dummyPlayerSyncList = new DummyPlayerSyncList();
-    public static DummyPlayerScript dummyPlayerScript;
+    public static NetworkIdentity networkIdentity;
+    public static GameObject player;
 
     private void Start() {
-        if (isServer == true) {
-            if (gameObject.GetComponent<NetworkIdentity>().isLocalPlayer) {
-                dummyPlayerScript = this;
-            }
-            serverStart(gameObject);
+        if (gameObject.GetComponent<NetworkIdentity>().isLocalPlayer == true) {
+            player = gameObject;
+            networkIdentity = player.GetComponent<NetworkIdentity>();
         }
-        return;
-    }
-
-    [Server]
-    private void serverStart(GameObject _gameObject) {
-        DummyPlayer dummyPlayer = new DummyPlayer {
-            networkIdentity = _gameObject.GetComponent<NetworkIdentity>()
-        };
-        dummyPlayer.index = dummyPlayerSyncList.Count;
-        dummyPlayerSyncList.Add(dummyPlayer);
-        if (dummyPlayerScript != _gameObject.GetComponent<DummyPlayerScript>()) {
-            clientRPCSuicide(_gameObject);
-        }
-        targetRPCReturnIndex(dummyPlayer.networkIdentity.connectionToClient, dummyPlayer.index);
-        return;
-    }
-
-    [ClientRpc]
-    private void clientRPCSuicide(GameObject _gameObject) {
-        Destroy(_gameObject.GetComponent<DummyPlayerScript>());
-        return;
-    }
-
-    [TargetRpc]
-    private void targetRPCReturnIndex(NetworkConnection networkConnection, int _index) {
-        _ = networkConnection;
-        dragAndDropScript.index = _index;
         return;
     }
 }
