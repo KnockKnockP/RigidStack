5f7969d1deca204c074fb63c99dfc034905d7e81
KnockKnockP <memeboi420666@gmail.com>
2020-10-19 13:00:21 +0900

    Add kicking function.

--- RigidStackSource/RigidStack/Assets/prefabs/level/DummyPlayer/Scripts/DummyPlayerScript.cs
+++ RigidStackSource/RigidStack/Assets/prefabs/level/DummyPlayer/Scripts/DummyPlayerScript.cs
@@ -2,15 +2,19 @@
 using UnityEngine;
 
 public class DummyPlayerScript : NetworkBehaviour {
+    public static DummyPlayerScript dummyPlayerScript = null;
     public static NetworkIdentity networkIdentity;
     public static GameObject player;
 
     private void Start() {
+        if (dummyPlayerScript == null) {
+            dummyPlayerScript = this;
+        }
         NetworkIdentity _networkIdentity = GetComponent<NetworkIdentity>();
         if (_networkIdentity.isLocalPlayer == true) {
             player = gameObject;
             networkIdentity = _networkIdentity;
-            gameObject.name = (LoadedPlayerData.playerData.name + "'s player.");
+            player.name = (LoadedPlayerData.playerData.name + "'s player.");
         }
         DontDestroyOnLoad(gameObject);
         return;
--- RigidStackSource/RigidStack/Assets/prefabs/level/gameManager/scripts/multiplayerLobbyScript.cs
+++ RigidStackSource/RigidStack/Assets/prefabs/level/gameManager/scripts/multiplayerLobbyScript.cs
@@ -1,17 +1,19 @@
 ﻿using Mirror;
+using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class multiplayerLobbyScript : NetworkBehaviour {
     //Variables for maintaining the lobby.
     private ushort usuablePort;
+    private List<string> playerNames = new List<string>();
     private TelepathyTransport telepathyTransport;
     private NetworkManager networkManager;
 
     //Variables for maintaining the lobby user interface.
     [SerializeField] private Text roomCodeText = null, statusText = null;
-    [SerializeField] private Button startButton = null;
+    [SerializeField] private Button startButton = null, kickButton = null;
+    [SerializeField] private Dropdown selectPlayerDropdownMenu = null;
     [SerializeField] private GameObject lobbyPanel = null;
 
     private void Awake() {
@@ -67,6 +69,23 @@ public class multiplayerLobbyScript : NetworkBehaviour {
         }
         return;
     }
+
+    [Command(ignoreAuthority = true)]
+    private void commandUpdatePlayerList(string playerName) {
+        List<string> temp = new List<string>();
+        foreach (string _playerName in playerNames) {
+            if (_playerName != null) {
+                temp.Add(_playerName);
+            }
+        }
+        playerNames = temp;
+        if (playerName != null) {
+            playerNames.Add(playerName);
+        }
+        selectPlayerDropdownMenu.ClearOptions();
+        selectPlayerDropdownMenu.AddOptions(playerNames);
+        return;
+    }
     #endregion
 
     #region Joining the multiplayer lobby.
@@ -80,8 +99,12 @@ public class multiplayerLobbyScript : NetworkBehaviour {
     public override void OnStartClient() {
         base.OnStartClient();
         commandUpdateTexts(false);
-        if (isServer) {
+        if (isServer == true) {
             startButton.gameObject.SetActive(true);
+            kickButton.gameObject.SetActive(true);
+            selectPlayerDropdownMenu.gameObject.SetActive(true);
+        } else if (isClientOnly == true) {
+            commandUpdatePlayerList(LoadedPlayerData.playerData.name);
         }
         lobbyPanel.SetActive(true);
         return;
@@ -112,6 +135,37 @@ public class multiplayerLobbyScript : NetworkBehaviour {
     private void clientRPCCancelMultiplayerLobby() {
         networkManager.StopClient();
         lobbyPanel.SetActive(false);
+        return;
+    }
+    #endregion
+
+    #region Kicking a player.
+    [Server]
+    public void kickPlayer() {
+        getKicked(playerNames[selectPlayerDropdownMenu.value]);
+        return;
+    }
+
+    [ClientRpc]
+    private void getKicked(string playerNameToKick) {
+        if (LoadedPlayerData.playerData.name == playerNameToKick) {
+            commandRemoveName(playerNameToKick);
+            commandUpdateTexts(true);
+            lobbyPanel.SetActive(false);
+            networkManager.StopClient();
+        }
+        return;
+    }
+
+    [Command(ignoreAuthority = true)]
+    private void commandRemoveName(string name) {
+        for (int i = 0; i < playerNames.Count; i++) {
+            if (name == playerNames[i]) {
+                playerNames[i] = null;
+                break;
+            }
+        }
+        commandUpdatePlayerList(null);
         return;
     }
     #endregion
