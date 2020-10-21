759e3678d0b9c0f419666be94908ae12d5aee11e
KnockKnockP <memeboi420666@gmail.com>
2020-10-21 18:51:59 +0900

    Fix bugs.

--- RigidStackSource/RigidStack/Assets/prefabs/level/DummyPlayer/Scripts/DummyPlayerScript.cs
+++ RigidStackSource/RigidStack/Assets/prefabs/level/DummyPlayer/Scripts/DummyPlayerScript.cs
@@ -2,26 +2,26 @@
 using UnityEngine;
 
 public class DummyPlayerScript : NetworkBehaviour {
-    public static bool didNameChecking = false;
+    public static bool loadedAllThings = false;
     public static DummyPlayerScript dummyPlayerScript = null;
     public static NetworkIdentity networkIdentity;
     public static GameObject player;
 
+    private void Awake() {
+        loadedAllThings = false;
+        return;
+    }
+
     private void Start() {
-        if (dummyPlayerScript == null) {
-            dummyPlayerScript = this;
-        }
+        DontDestroyOnLoad(gameObject);
         NetworkIdentity _networkIdentity = GetComponent<NetworkIdentity>();
         if (_networkIdentity.isLocalPlayer == true) {
+            dummyPlayerScript = this;
             player = gameObject;
-            networkIdentity = _networkIdentity;
             player.name = (LoadedPlayerData.playerData.name + "'s player.");
-            if (isClientOnly == true) {
-                FindObjectOfType<multiplayerLobbyScript>().commandNameCheck(LoadedPlayerData.playerData.name, networkIdentity);
-                didNameChecking = true;
-            }
+            networkIdentity = _networkIdentity;
+            loadedAllThings = true;
         }
-        DontDestroyOnLoad(gameObject);
         return;
     }
 }
