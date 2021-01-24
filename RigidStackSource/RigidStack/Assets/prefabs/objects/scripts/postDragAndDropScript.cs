ce86a8fb5270f950a8245f3c62f7b66daa869e8f
KnockKnockP <memeboi420666@gmail.com>
2021-01-24 23:58:02 +0900

    Add an offset when dragging an object.

--- RigidStackSource/RigidStack/Assets/prefabs/objects/scripts/postDragAndDropScript.cs
+++ RigidStackSource/RigidStack/Assets/prefabs/objects/scripts/postDragAndDropScript.cs
@@ -3,24 +3,44 @@ using System;
 using UnityEngine;
 
 public class postDragAndDropScript : NetworkBehaviour {
+    private Vector2 offset = Vector2.zero;
+
     //The object we are going to manipulate.
+    private Transform thisTransform;
     [NonSerialized] public GameObject thisGameObject;
 
     private void Awake() {
         thisGameObject = gameObject;
+        thisTransform = gameObject.transform;
         return;
     }
 
     #region Dragging the placed object.
+    private void OnMouseDown() {
+        offset = CalculateOffset(GetMouseToWorldPoint(), thisTransform.position);
+        return;
+    }
+
     private void OnMouseDrag() {
-        commandDragObject(thisGameObject, sharedMonobehaviour._sharedMonobehaviour.mainCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10)));
+        commandDragObject(GetMouseToWorldPoint());
         return;
     }
 
     [Command(ignoreAuthority = true)]
-    private void commandDragObject(GameObject _gameObject, Vector3 position) {
-        _gameObject.transform.position = position;
+    private void commandDragObject(Vector3 mouseinput) {
+        Vector3 newPosition = new Vector3((mouseinput.x - offset.x), (mouseinput.y - offset.y), 0f);
+        thisTransform.position = newPosition;
         return;
     }
     #endregion
+
+    private static Vector3 GetMouseToWorldPoint() {
+        Vector3 mouseInput = Input.mousePosition;
+        return sharedMonobehaviour._sharedMonobehaviour.mainCamera.ScreenToWorldPoint(new Vector3(mouseInput.x, mouseInput.y, 10f));
+    }
+
+    private static Vector2 CalculateOffset(Vector3 mousePosition, Vector3 gameObjectPosition) {
+        Vector2 offset = new Vector2((mousePosition.x - gameObjectPosition.x), (mousePosition.y - gameObjectPosition.y));
+        return offset;
+    }
 }
